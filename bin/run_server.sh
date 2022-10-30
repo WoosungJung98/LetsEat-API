892386703c7b26305fed3b56176761992c7228f6
WoosungJung98 <woosung.william.jung@gmail.com>
2022-10-30 08:51:47 -0400

    fix gunicorn command syntax error

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 NUM_VCPUS=$(grep -c ^processor /proc/cpuinfo)
-gunicorn -w $(NUM_VCPUS) -b "0.0.0.0:$1" "main:create_app()"
+gunicorn -w $((NUM_VCPUS)) -b "0.0.0.0:$1" "main:create_app()"
