4d35e1adfdc8d695cb103e45a2caf37a7811b7c8
WoosungJung98 <woosung.william.jung@gmail.com>
2022-11-03 02:47:06 -0400

    Optimize formula for gunicorn worker deployment

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-NUM_VCPUS=$(grep -c ^processor /proc/cpuinfo)
-gunicorn -w $((NUM_VCPUS)) -b "0.0.0.0:$1" "main:create_app()" -D
+NUM_CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+gunicorn -w $((NUM_CORES * 2 + 1)) -b "0.0.0.0:$1" "main:create_app()" -D
