6161e079c82274abac8e4eeec20dfd8122629c0e
WoosungJung98 <woosung.william.jung@gmail.com>
2022-11-04 01:50:52 -0400

    decrease core count for gunicorn workers

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 NUM_CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
-gunicorn -w $((NUM_CORES * 2 + 1)) -b "0.0.0.0:$1" "main:create_app()" -D
+gunicorn -w $((NUM_CORES * 2)) -b "0.0.0.0:$1" "main:create_app()" -D
